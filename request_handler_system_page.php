19e5817e2415b58e5c1cfaa775d5138e86df9ca0
Anjay56 <71063370+Anjay56@users.noreply.github.com>
2021-04-04 20:21:08 +0700

    Design the Request Handler System Page [Still in progress]
    Only has simple HTML and CSS

---
+++ request_handler_system_page.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section>
+        <h1>Request Handler System</h1>
+
+        <table class="request-table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Selected Training Type</th>
+                    <th>Status</th>
+                    <th>Date Created</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Abeng</td>
+                    <td>Negotiation skills</td>
+                    <td>Pending</td>
+                    <td>4 April 2021</td>
+                </tr>
+                <tr>
+                    <td>Irwan</td>
+                    <td>Presentation skills</td>
+                    <td>Pending</td>
+                    <td>4 April 2021</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</body>
+</html>
