c0d44fdd06fee002fbea9c5c1823b6e8c74e26c5
Anjay56 <71063370+Anjay56@users.noreply.github.com>
2021-04-05 19:39:50 +0700

    Add training_type.sql table

---
+++ mysql_tables/training_type.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2021 at 01:48 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `etmp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `training_type`
+--
+
+CREATE TABLE `training_type` (
+  `training_type_id` int(11) UNSIGNED NOT NULL,
+  `training_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `training_type`
+--
+
+INSERT INTO `training_type` (`training_type_id`, `training_type`) VALUES
+(1, 'Leadership & Communication skills'),
+(2, 'Negotiation skills'),
+(3, 'Presentation skills');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `training_type`
+--
+ALTER TABLE `training_type`
+  ADD PRIMARY KEY (`training_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `training_type`
+--
+ALTER TABLE `training_type`
+  MODIFY `training_type_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
