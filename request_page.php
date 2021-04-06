eeff70e01f1848112b8ee91fa1acd32d93f8e77c
Anjay56 <71063370+Anjay56@users.noreply.github.com>
2021-04-06 18:29:59 +0700

    Design the Request Handler System Page
    Complete but still needs some testing

---
+++ request_page.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/request_handler_system.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="request-detail-section">
+        <h1>Request Details</h1>
+        <p class="container-cancel-button">
+            <button class="cancel-button">Cancel Request</button>
+        </p>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "etmp";
+
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            if ($conn->connect_errno) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            if (isset($_COOKIE["request_id"])) {
+                $request_id = $_COOKIE["request_id"];
+
+                $sql = "SELECT * FROM request_form WHERE form_id = $request_id";
+                $result = $conn->query($sql);
+
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<h5>" . "First Name" . "</h5>";
+                    echo "<p>"  . $row["fname"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "Last Name" . "</h5>";
+                    echo "<p>"  . $row["lname"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "Phone" . "</h5>";
+                    echo "<p>"  . $row["phone"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "Address" . "</h5>";
+                    echo "<p>"  . $row["address"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "City" . "</h5>";
+                    echo "<p>"  . $row["city"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "State" . "</h5>";
+                    echo "<p>"  . $row["state"] . "</p>";
+                    echo "<hr>";
+
+                    echo "<h5>" . "Training Type" . "</h5>";
+            
+                    $data_int = (int)$row['training_type_id'];
+                            
+                    $sql_2 = "SELECT * FROM training_type WHERE training_type_id = $data_int";
+                    $result2 = $conn->query($sql_2);
+                    
+                    while ($row = mysqli_fetch_assoc($result2)) {
+                        echo "<p>" . $row['training_type_name'] . "</p>";
+                    }
+                    
+                    echo "<hr>";
+                }
+            }
+        ?>
+    </div>
+        
+    <div class="search-training-section">
+        <h1>Search Trainings</h1>
+        <form method="post">
+            <label class="label-training-type" for="search">Training Type</label>
+            <input class="training-search-bar" type="text" placeholder="Search..." name="search"/>
+            <button class='search-button' type="submit"><i class="fa fa-search"></i></button>   
+        </form>
+
+        <table class="search-training-table">
+            <tr>
+                <th>Training Name</th>
+                <th>Training Type</th>
+                <th>Price</th>
+            </tr>
+
+        </table>
+
+        <p class="container-send-training-button">
+            <button class="send-training-button">Cancel Request</button>
+        </p>
+    </div>
+    
+    
+
+    <script>
+        var p = document.getElementById("test");
+    </script>
+</body>
+</html>
--- requests_handler_system_page.php
+++ requests_handler_system_page.php
@@ -1,17 +1,20 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/request_handler_system.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
     <section>
         <h1>Request Handler System</h1>
-
         <?php
             $servername = "localhost";
             $username = "root";
@@ -24,7 +27,7 @@
                 die("Connection failed: " . $conn->connect_error);
             }
 
-            $sql = "SELECT fname, lname, selected_training_type, request_status, request_date FROM request_form";
+            $sql = "SELECT * FROM request_form";
             $result = $conn->query($sql);
 
             mysqli_num_rows($result);
@@ -32,6 +35,7 @@
 
 
             $numOfHeaders = 5;
+
 
             if (($result)) {
                 echo "<table class='request-table'> <tr>";
@@ -42,12 +46,24 @@
                     }
                     echo "</tr>";
 
-                    while ($rows = mysqli_fetch_assoc($result)) {
+                    while ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
                         echo "<tr>";
-                        foreach ($rows as $data) {
-                            echo "<td>" . $data . "</td>";
+                        
+                        echo "<td>" . $rows['fname'] . "</td>";
+                        echo "<td>" . $rows['lname'] . "</td>";
+                        $data_int = (int)$rows['training_type_id'];
+                            
+                        $sql_2 = "SELECT * FROM training_type WHERE training_type_id = $data_int";
+                        $result2 = $conn->query($sql_2);
+                        
+                        while ($row = mysqli_fetch_assoc($result2)) {
+                            echo "<td>" . $row['training_type_name'] . "</td>";
                         }
-                        echo "<td><button class='button-training-request'>Manage</button></td>";
+
+                        echo "<td>" . $rows['request_status'] . "</td>";
+                        echo "<td>" . $rows['request_date'] . "</td>";
+                        
+                        echo "<td><button class='button-training-request' name='lala' type='submit'" . " value= " . $rows['form_id'] . ">Manage</button></td>";
                         echo "</tr>";
                     }
                 }
@@ -58,6 +74,7 @@
             }
         ?>
 
-    </section>
+    </section>    
+    <script src="scripts/requestHandler.js"></script>
 </body>
 </html>
