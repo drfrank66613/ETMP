fe24d4213785985c8310c00b0dfbf42566103ccc
Irwan Ngo <khairuddinumar@gmail.com>
2021-04-06 20:27:14 +0800

    Update Notification Page

--- notification.php
+++ notification.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="styles/notification.css">
+    <link rel="stylesheet" href="styles/notify.css">
     <meta charset="utf-8">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
@@ -24,12 +24,12 @@
         die("Connection failed:". $conn->connect_error);
       }
 
-      $sql = "SELECT notification_id, user_id, title, content, date_received FROM notifications";
+      $sql = "SELECT * FROM notifications";
       $result = $conn->query($sql);
 
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()){
-          echo "<tr><td><h3>" . $row["title"] . "</h3></td><td class='dates'>" . $row["date_received"] . "</td></tr><tr><td class='contents'>" . $row["content"] . "</td></tr>";
+          echo "<tr class='the_row'><td><h3>" . $row["title"] . "</h3></td><td class='dates' rowspan='2'>" . $row["date_received"] . "</td></tr><tr class='the_row'><td class='contents'>" . $row["content"] . "</td></tr>";
         }
         echo "</table";
       } else { echo "No Data"; }
