268fa2cb1c313b8caf4f7ba3100df3b417e0f168
Irwan Ngo <khairuddinumar@gmail.com>
2021-04-21 20:21:05 +0800

    Updated Notification Page

--- notification.php
+++ notification.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="styles/notify.css">
+    <link rel="stylesheet" href="./styles/notify.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -21,25 +21,24 @@
         <h4 class="appdesc">ONE OF THE LARGEST TRAINING PROVIDER IN SARAWAK</h4>
     </div>
     <div class="navbar">
-        <a href="client_homepage.php">Home</a>
+        <a class="active" href="client_homepage.php">Home</a>
         <a href="#news">My Training</a>
         <a href="client_about_page.php">About</a>
         <div class="rightnavbar">
-            <a class="active" class="notif" href="notification.php"><i class="fa fa-bell"></i></a>
+            <a class="notif" href="notification.php"><i class="fa fa-bell"></i></a>
             <div class="dropdown">
                 <button class="profile">Welcome, <?php echo $_SESSION['username']; ?><i class="fa fa-sort-down" ></i></button>
                 <div class="dropdown-content">
                   <a href="#">Edit Profile</a>
                   <a href="logout_session.php?logout">Log Out</a>
                 </div>
-              </div> 
-        </div>  
+              </div>
+        </div>
     </div>
     <!---->
-    
+
     <h1 class="notification_title">Notifications</h1>
     <table class="notifications_table">
-
       <?php
       $host = "localhost";
       $dbUsername = "root";
@@ -52,15 +51,20 @@
 
       $sql = "SELECT * FROM notifications";
       $result = $conn->query($sql);
-
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()){
-          echo "<tr class='the_row'><td><h3>" . $row["title"] . "</h3></td><td class='dates' rowspan='2'>" . "<p>Date Received</p>" . $row["date_received"] . "</td></tr><tr class='the_row'><td class='contents'>" . $row["content"] . "</td></tr>";
-        }
-        echo "</table";
-      } else { echo "No Data"; }
-      $conn->close();
-      ?>
+       echo "<tr>
+        <td class ='notification_topic'><h3>" . $row["title"] . "</h3></td>
+        <td class = 'date_notification_received'>" . $row["date_received"] . "</td>
+      </tr>
+      <tr class = 'table_rows'>
+        <td colspan='2' class ='second_row'><span>" .$row["content"] . "</span</td>
+
+      </tr>";
+    }
+  }else { echo "No Data"; }
+  $conn->close();
+  ?>
     </table>
 
   </body>
