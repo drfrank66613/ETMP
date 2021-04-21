d94380c8b430100d0fffd5f47da1aab3d14d152c
Anjay56 <71063370+Anjay56@users.noreply.github.com>
2021-04-21 15:26:45 +0700

    Update connect_request_to_database.php

--- connect_request_to_database.php
+++ connect_request_to_database.php
@@ -1,4 +1,9 @@
 <?php
+
+
+session_start();
+
+$userid = 0;
 $trainingid = 'NOT SET';
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
@@ -29,11 +34,19 @@ $time = $_POST['time'];
     while ($row = mysqli_fetch_assoc($result_training)) {
       $trainingid = $row['training_type_id'];
     }
+    
+    
+    $username = $_SESSION['username'];
+    $query_user_id = "SELECT * FROM user_information WHERE '$username' = username";
+    $result_user_id = mysqli_query($conn, $query_user_id);
+    $row = mysqli_fetch_assoc($result_user_id);
+    $userid = $row['id'];
 
 
     $sql = "INSERT INTO request_form (user_id, training_type_id, fname, lname, phone, email, address, city, state, training_venue, training_date, training_time)
-    VALUES('1','$trainingid', '$firstname', '$lastname', '$phone', '$email', '$address', '$city', '$state', '$venue', '$date', '$time')";
+    VALUES('$userid','$trainingid', '$firstname', '$lastname', '$phone', '$email', '$address', '$city', '$state', '$venue', '$date', '$time')";
     if (mysqli_query($conn, $sql)) {
+      echo $userid;
       echo "Training Request Form Submitted";
       }
     else {
