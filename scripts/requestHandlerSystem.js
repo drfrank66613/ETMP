f13128aaacf84ecc45bfe72bc7f0c991bedbdc22
Anjay56 <71063370+Anjay56@users.noreply.github.com>
2021-05-11 20:48:35 +0700

    Update admin side to handle Training Itinerary

--- scripts/requestHandlerSystem.js
+++ scripts/requestHandlerSystem.js
@@ -5,11 +5,19 @@ window.onload = function() {
     }
 }
 
-var buttons = document.querySelectorAll(".button-training-request");
+var requestButtons = document.querySelectorAll(".button-training-request");
+var itineraryButtons = document.querySelectorAll(".button-training-itinerary");
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click",  function() {
+for (var i = 0; i < requestButtons.length; i++) {
+    requestButtons[i].addEventListener("click",  function() {
         location.href = "request_page.php";
         document.cookie = "request_id=" + this.getAttribute("value") + "; path=/";
+    });
+}
+
+for (var i = 0; i <itineraryButtons.length; i++) {
+    itineraryButtons[i].addEventListener("click", function() {
+        location.href = "admin_itinerary_management_page.php";
+        document.cookie = "itinerary_id=" + this.getAttribute("value") + "; path=/";
     });
 }
