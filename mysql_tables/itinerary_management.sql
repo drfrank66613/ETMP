e2e1e039c9df3c94a4cb9ced044dd0ae31275508
George Kennedy <dm66613@gmail.com>
2021-05-12 18:45:25 +0800

    Updated itinerary_management, training_itinerary & training_workshop tables

--- mysql_tables/itinerary_management.sql
+++ mysql_tables/itinerary_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2021 at 03:21 PM
+-- Generation Time: May 12, 2021 at 12:42 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
--- mysql_tables/training_itinerary.sql
+++ mysql_tables/training_itinerary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2021 at 03:21 PM
+-- Generation Time: May 12, 2021 at 12:42 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -30,18 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `training_itinerary` (
   `training_itinerary_id` int(11) UNSIGNED NOT NULL,
   `training_id` int(11) UNSIGNED NOT NULL,
-  `day` varchar(10) NOT NULL,
-  `activity` text NOT NULL,
-  `date` varchar(30) NOT NULL,
-  `time` varchar(30) NOT NULL
+  `form_id` int(10) UNSIGNED NOT NULL,
+  `training_itinerary_status` varchar(100) NOT NULL,
+  `current_used_date` varchar(30) NOT NULL,
+  `current_used_time` varchar(30) NOT NULL,
+  `modification_date` varchar(30) NOT NULL,
+  `modification_time` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `training_itinerary`
 --
 
-INSERT INTO `training_itinerary` (`training_itinerary_id`, `training_id`, `day`, `activity`, `date`, `time`) VALUES
-(1, 1, 'day 1', '9:30 Opening Ceremony\r\n10:30 Ice Breaking', '2021-05-10', '09:30');
+INSERT INTO `training_itinerary` (`training_itinerary_id`, `training_id`, `form_id`, `training_itinerary_status`, `current_used_date`, `current_used_time`, `modification_date`, `modification_time`) VALUES
+(1, 1, 6, 'Unconfirmed', '2021-05-10', '10:00 AM', '-', '-');
 
 --
 -- Indexes for dumped tables
@@ -52,7 +54,8 @@ INSERT INTO `training_itinerary` (`training_itinerary_id`, `training_id`, `day`,
 --
 ALTER TABLE `training_itinerary`
   ADD PRIMARY KEY (`training_itinerary_id`),
-  ADD KEY `fk_training_workshop` (`training_id`);
+  ADD KEY `fk_training_workshop` (`training_id`),
+  ADD KEY `fk_request_form` (`form_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -72,6 +75,7 @@ ALTER TABLE `training_itinerary`
 -- Constraints for table `training_itinerary`
 --
 ALTER TABLE `training_itinerary`
+  ADD CONSTRAINT `fk_request_form` FOREIGN KEY (`form_id`) REFERENCES `request_form` (`form_id`),
   ADD CONSTRAINT `fk_training_workshop` FOREIGN KEY (`training_id`) REFERENCES `training_workshop` (`training_id`);
 COMMIT;
 
